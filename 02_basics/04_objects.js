321e74b85875b60ac7bd8b66b76da2fef3c861ef
Sayali <thengalsayali05@gmail.com>
2024-02-28 14:05:50 +0530

    objs

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -53,29 +53,3 @@ users[1].email
 // console.log(Object.entries(tinderUser));
 
 // console.log(tinderUser.hasOwnProperty('isLoggedIn'));
-
-
-const course = {
-    coursename: "js in hindi",
-    price: "999",
-    courseInstructor: "hitesh"
-}
-
-// course.courseInstructor
-
-const {courseInstructor: instructor} = course
-
-// console.log(courseInstructor);
-console.log(instructor);
-
-// {
-//     "name": "hitesh",
-//     "coursename": "js in hindi",
-//     "price": "free"
-// }
-
-[
-    {},
-    {},
-    {}
-]
