ecc0b0ecb6319930ec16e8567b6e0c05609322b2
Sayali <thengalsayali05@gmail.com>
2024-02-29 21:39:46 +0530

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -36,4 +36,36 @@ function loginUserMessage(username = "sam"){
 }
 
 // console.log(loginUserMessage("Sayali"))
-console.log(loginUserMessage("sayali"))
+// console.log(loginUserMessage("Sayali"))
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "Sayali",
+    price: 199
+}
+
+function handleObject(anyObject){
+    console.log(`User name is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 100, 600]));
