badb722cdd3f5544f5d2a560139d98d18ca9c319
Sayali <thengalsayali05@gmail.com>
2024-03-16 04:42:37 +0530

    js-projects

--- 07_projects/3-DigitalClock/index.js
+++ 07_projects/3-DigitalClock/index.js
@@ -1,9 +1,8 @@
-const clock = document.getElementById('clock');
-// const clock = document.querySelector('#clock')
+const clock = document.getElementById('clock')
+// document.querySelector('#clock')
 
 setInterval(function () {
     let date = new Date();
     // console.log(date.toLocaleTimeString());
     clock.innerHTML = date.toLocaleTimeString();
-}, 1000);
-
+}, 1000)
