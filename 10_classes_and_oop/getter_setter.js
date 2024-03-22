77d9a92c9f4e126a4ebe9f100a20e385dafa5d47
Sayali <thengalsayali05@gmail.com>
2024-03-23 03:14:18 +0530

    getter and setters

---
+++ 10_classes_and_oop/getter_setter.js
@@ -0,0 +1,25 @@
+class User {
+    constructor(email, password){
+        this.email = email;
+        this.password = password
+    }
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+        this._email = value
+    }
+
+    get password(){
+        return `${this._password}hitesh`
+    }
+
+    set password(value){
+        this._password = value
+    }
+}
+
+const sayali = new User("s@sayali.ai", "abc")
+console.log(sayali.password);
+console.log(sayali.email);
